48157408b6aa22748548aaeaa300227d96c65155
Cesar <cesar.yt2015@gmail.com>
2020-03-17 22:39:31 -0600

    mi primer commit

---
+++ Apps.cpp
@@ -0,0 +1,157 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<conio.h>
+
+main()
+{
+   inicio:
+	printf("			 *****Evernot*****\n"); 
+	printf("\n\n");
+	printf("Descripcion: Toma nostas tan rapido que no dejaras de hacerlo");
+	printf("\n\n");
+	printf("Vendedor:  Evernot");
+	printf("\n\n");
+	printf("Categoria: Productividad");
+	printf("\n\n");
+	printf("Vercion: 3.1.5");
+	printf("\n\n");
+	printf("Tamaño: 850 MB");
+	printf("\n\n");
+	printf("Compatibilidad:  Requiere iOS 7.0 o posterior. Compatible con iPhone, iPad y iPod touch. ");
+	printf("\n\n");
+	printf("Idioma:   	Español");
+	printf("\n\n");
+	printf("Precio:   *Gratis pero con Suscribcion*");
+	printf("\n\n");
+FILE *fra;
+                                int c;
+                                char factura[100]= "     ******Factura*****\n\n";
+                                char app[100]= "     Applicacion: Evernot\n";
+                                char vendedor[100]= "     Vendedor: Evernot\n";
+                                char categoria[100]= "     Categoria: Productividad\n";
+                                char vercion[50]= "     Vercion: 3.1.5\n";
+                                char tamano[100]= "     Tamaño: 850 MB\n";
+                                char idioma[50]= "     Idioma: Español\n";
+                                char precio[100]= "     Precio de App con IVA: *Gratis pero con Suscribcion*\n";
+                                char numero[100]= "     Cuenta Bancaria: 1579696321453658\n\n";
+                                char publicidad[100]= "     ***Recuerda Disfrutar de las Mejores Apss solo en IOS***\n\n\n";
+                                fra = fopen("Factura.txt", "w+");
+								if(fra!=NULL){
+														
+								printf("			 ***Desea Comprar la App 1=Si 0=No***\n\n");
+									scanf("%i", &c);
+									if (c=1){
+									fprintf(fra, factura);	
+									fprintf(fra, app);
+						  			fprintf(fra, vendedor);
+						  			fprintf(fra, categoria);
+						  			fprintf(fra, vercion);
+						  			fprintf(fra, tamano);
+						  			fprintf(fra, idioma);
+						  			fprintf(fra, precio);
+						  			fprintf(fra, numero);
+						  			
+}}
+	 fclose(fra);
+	 int d;
+	 printf("			 ---Descargar ahora 1=Si 2=No---\n\n");
+	 scanf("%i", &d);
+	 if (d==1){
+	 	for(int i=0; i<1; i++){ 
+             
+            printf("\n  [*]=== - 10%% "); 
+            system("cls"); 
+            printf("\n  [*]=== - 10%% "); 
+            system("cls"); 
+            printf("\n  [*]=== - 10%% "); 
+            system("cls"); 
+            printf("\n  [*]=== - 10%% "); 
+
+
+            system("cls"); 
+            printf("\n  [*]===== | 20%% "); 
+            system("cls"); 
+            printf("\n  [*]===== | 20%% "); 
+            system("cls"); 
+            printf("\n  [*]===== | 20%% "); 
+            system("cls"); 
+            printf("\n  [*]===== | 20%% "); 
+
+            system("cls"); 
+            printf("\n  [*]======= / 30%% "); 
+            system("cls"); 
+            printf("\n  [*]======= / 30%% "); 
+            system("cls"); 
+            printf("\n  [*]======= / 30%% "); 
+
+
+            system("cls"); 
+            printf("\n  [*]======== - 40%% "); 
+            system("cls"); 
+            printf("\n  [*]======== - 40%% "); 
+            system("cls"); 
+            printf("\n  [*]======== - 40%% "); 
+            system("cls"); 
+            printf("\n  [*]======== - 40%% "); 
+
+            system("cls"); 
+            printf("\n  [*]========== | 50%% \n"); 
+            system("cls"); 
+            printf("\n  [*]========== | 50%% \n"); 
+            system("cls"); 
+            printf("\n  [*]========== | 50%% \n"); 
+            system("cls"); 
+            printf("\n  [*]========== | 50%% \n"); 
+
+            system("cls"); 
+            printf("\n  [*]=========== ""\" 60%% \n"); 
+            system("cls"); 
+            printf("\n  [*]=========== ""\" 60%% \n"); 
+            system("cls"); 
+            printf("\n  [*]=========== ""\" 60%% \n"); 
+
+            system("cls"); 
+            printf("\n  [*]============ | 70%% \n"); 
+            system("cls"); 
+            printf("\n  [*]============ | 70%% \n"); 
+            system("cls"); 
+            printf("\n  [*]============ | 70%% \n"); 
+            system("cls"); 
+            printf("\n  [*]============ | 70%% \n"); 
+
+            system("cls"); 
+            printf("\n  [*]============= / 80%% \n"); 
+            system("cls"); 
+            printf("\n  [*]============= / 80%% \n"); 
+            system("cls"); 
+            printf("\n  [*]=============/ 80%% \n"); 
+
+            
+            system("cls"); 
+            printf("\n  [*]============== - 90%% "); 
+            system("cls"); 
+            printf("\n  [*]============== - 90%% "); 
+
+            
+            system("cls"); 
+            printf("\n  [*]=============== | 100%% \n"); 
+            system("cls"); 
+            printf("\n  [*]=============== | 100%% \n"); 
+            system("cls"); 
+            printf("\n  [*]=============== | 100%% \n"); 
+            } 
+            printf("\n\n");
+            printf("		********Descarga Completa********\n\n");
+            int m;
+            printf("		--Desea Volver al Menu Anterior :) 1=Si 0=No --\n\n");
+	 		scanf("%i", &m);
+	 		if (m==1){
+	 			goto sus;
+			 }
+	 
+}
+return 0;
+}
+
